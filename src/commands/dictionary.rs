9fde9c2cfaf3adfed8f66f10a3d214c05416f8e6
Jacob Helwig <jacob@technosorcery.net>
2017-10-05 14:11:41 -0700

    Make dictionary facts per-Discord "Guild" instead of global

--- src/commands/dictionary.rs
+++ src/commands/dictionary.rs
@@ -1,5 +1,6 @@
 use redis;
 use redis::Commands;
+use serenity::model::GuildId;
 use std::cell::RefCell;
 use std::error::Error;
 
@@ -17,19 +18,31 @@ thread_local! {
 }
 
 command!(get_dictionary_entry(_ctx, msg, args) {
-    let key = args.single::<String>().unwrap();
+    let guild_id = msg.guild_id();
+
+    let short_key = args.single::<String>().unwrap();
+    let key;
+    match guild_id {
+        None => {
+            let _ = msg.channel_id.say("Sorry, I can't do that here.");
+            return Ok(());
+        },
+        Some(GuildId(g)) => {
+            key = namespaced_dictionary_key(&short_key, g);
+        },
+    };
 
     let result = execute_redis_command(RedisCommand::Get, &key, None::<&String>);
 
     let response = match result {
         Ok(r) => {
             match r {
-                Some(value) => format!("{key} is {value}", key=key, value=value),
-                None => format!("{key} is not set", key=key),
+                Some(value) => format!("{key} is {value}", key=short_key, value=value),
+                None => format!("{key} is not set", key=short_key),
             }
         },
         Err(e) => {
-            format!("Unable to retrieve {key}: {error}", key=key, error=e)
+            format!("Unable to retrieve {key}: {error}", key=short_key, error=e)
         },
     };
 
@@ -37,17 +50,29 @@ command!(get_dictionary_entry(_ctx, msg, args) {
 });
 
 command!(set_dictionary_entry(_ctx, msg, args) {
-    let key = args.single::<String>().unwrap();
+    let guild_id = msg.guild_id();
+
+    let short_key = args.single::<String>().unwrap();
+    let key;
+    match guild_id {
+        None => {
+            let _ = msg.channel_id.say("Sorry, I can't do that here.");
+            return Ok(());
+        },
+        Some(GuildId(g)) => {
+            key = namespaced_dictionary_key(&short_key, g);
+        },
+    };
     let value = args.single::<String>().unwrap();
 
     let result = execute_redis_command(RedisCommand::Set, &key, Some(&value));
 
     let response = match result {
         Ok(_) => {
-            format!("Ok! {key} is now {value}", key=key, value=value)
+            format!("Ok! {key} is now {value}", key=short_key, value=value)
         },
         Err(e) => {
-            format!("Unable to set {key} to {value}: {error}", key=key, value=value, error=e)
+            format!("Unable to set {key} to {value}: {error}", key=short_key, value=value, error=e)
         },
     };
 
@@ -57,15 +82,14 @@ command!(set_dictionary_entry(_ctx, msg, args) {
 fn execute_redis_command<'a, T>(cmd: RedisCommand, key: &String, val: Option<&'a T>) -> Result<Option<String>, String> where &'a T: redis::ToRedisArgs{
     REDIS_CONNECTION.with(|r| {
         let con: &redis::RedisResult<redis::Connection> = &*r.borrow();
-        let full_key = namespaced_dictionary_key(&key);
 
         match con {
             &Ok(ref c) => {
                 match cmd {
                     RedisCommand::Get => {
-                        match c.exists(full_key.clone()) {
+                        match c.exists(key.clone()) {
                             Ok(true) => {
-                                match c.get(full_key) {
+                                match c.get(key) {
                                     Ok(r) => Ok(Some(r)),
                                     Err(e) => Err(e.description().to_string()),
                                 }
@@ -75,7 +99,7 @@ fn execute_redis_command<'a, T>(cmd: RedisCommand, key: &String, val: Option<&'a
                         }
                     },
                     RedisCommand::Set => {
-                        match c.set(full_key, val) {
+                        match c.set(key, val) {
                             Ok(()) => Ok(None),
                             Err(e) => Err(e.description().to_string()),
                         }
@@ -87,6 +111,6 @@ fn execute_redis_command<'a, T>(cmd: RedisCommand, key: &String, val: Option<&'a
     })
 }
 
-fn namespaced_dictionary_key(key: &String) -> String {
-    format!("technobot::dictionary::{}", key)
+fn namespaced_dictionary_key(key: &String, guild_id: u64) -> String {
+    format!("technobot::dictionary::{guild_id}::{key}", key=key, guild_id=guild_id)
 }
